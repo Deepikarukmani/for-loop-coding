e0fee78e8bdfcd9a33287cf69561a2899a190c54
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-19 23:46:51 +0530

    Add files via upload

---
+++ for loop code with examples.py
@@ -0,0 +1,16 @@
+#for loop syntax:
+for i in "apple":
+    print(i)
+
+#for loop syntax:
+for python in "coding":
+    print(python)
+
+#for loop syntax:
+for i in range(5):
+    print(i)
+
+#pattern program
+a = [1, 2, 3, 4, 5]
+for i in a:
+    print(i *" *")
