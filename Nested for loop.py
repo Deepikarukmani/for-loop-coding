610fbb1e6df640699a3da903ab2a801be3be41fd
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-22 00:22:10 +0530

    Add files via upload

---
+++ Nested for loop.py
@@ -0,0 +1,10 @@
+#Nested for loop syntax:
+for i in range(0,5):
+    for j in range(1,3):
+        print(j, "apple")
+
+#Nested for loop syntax:
+for i in range(1,3):
+    print("Week:",i)
+    for j in range(1,4):
+        print("Day:",j)
