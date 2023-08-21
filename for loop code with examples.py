c29cb677ed00df72ffab0a35c4cc409a10a19dea
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-21 10:45:32 +0530

    for loop code with examples.py

--- for loop code with examples.py
+++ for loop code with examples.py
@@ -14,3 +14,22 @@ for i in range(5):
 a = [1, 2, 3, 4, 5]
 for i in a:
     print(i *" *")
+
+#for loop using "2" tables:
+for i in range(1, 11):
+    print(i,"x2 =", i*2)
+
+#for loop using "3" tables:
+for i in range(1, 21):
+    print(i, "x3 =", i*3)
+
+# Find even
+a = [1,2,3,4,5,6,7,8,9]
+for i in a:
+    if i%2 ==0:
+        print(i)
+
+# Find even using range function
+for i in range(1, 11):
+    if i%2 ==0:
+        print(i)
