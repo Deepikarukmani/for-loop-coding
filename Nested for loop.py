d0bce4ffba69a43ef7a9ee3fc95f061ff89951cc
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-22 00:49:41 +0530

    Update Nested for loop.py

--- Nested for loop.py
+++ Nested for loop.py
@@ -8,3 +8,14 @@ for i in range(1,3):
     print("Week:",i)
     for j in range(1,4):
         print("Day:",j)
+
+#pattern program:
+n= int(input())
+for i in range(n):
+        print("1" *n)
+
+#pattern program:
+for i in range(1,5):
+    print()
+    for j in range(1, i+1):
+        print(j,end="")
