75b96194a7ef26adcdde6a8cf5f28d87b213024c
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-22 22:33:42 +0530

    Update Nested for loop.py

--- Nested for loop.py
+++ Nested for loop.py
@@ -9,7 +9,16 @@ for i in range(1,3):
     for j in range(1,4):
         print("Day:",j)
 
-#pattern program:
+
+#Print the square pattern with the number '1' in R*R form based on the given integer R.
+# Sample Input :
+# 5
+# Sample Output :
+# 11111
+# 11111
+# 11111
+# 11111
+# 11111
 n= int(input())
 for i in range(n):
         print("1" *n)
@@ -18,4 +27,23 @@ for i in range(n):
 for i in range(1,5):
     print()
     for j in range(1, i+1):
+        print(j,end="")
+
+#pattern program:
+for i in range(1,5):
+    print()
+    for j in range(1, i+1):
+        print("* " ,end="")
+
+# Print the half pyramid pattern based on the given integer R.# Sample Input : 5
+# Sample Output :
+# 1
+# 12
+# 123
+# 1234
+# 12345
+n = int(input())
+for i in range(0,n+1):
+    print()
+    for j in range(1,i+1):
         print(j,end="")
