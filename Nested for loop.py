4e3986c007b23a91d0c99c8cc8183d25933ee74c
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-23 01:18:30 +0530

    Update Nested for loop.py

--- Nested for loop.py
+++ Nested for loop.py
@@ -10,7 +10,7 @@ for i in range(1,3):
         print("Day:",j)
 
 
-#Print the square pattern with the number '1' in R*R form based on the given integer R.
+question: #Print the square pattern with the number '1' in R*R form based on the given integer R.
 # Sample Input :
 # 5
 # Sample Output :
@@ -35,7 +35,7 @@ for i in range(1,5):
     for j in range(1, i+1):
         print("* " ,end="")
 
-# Print the half pyramid pattern based on the given integer R.# Sample Input : 5
+question: # Print the half pyramid pattern based on the given integer R.# Sample Input : 5
 # Sample Output :
 # 1
 # 12
@@ -49,7 +49,7 @@ for i in range(0,n+1):
         print(j,end="")
 
 
-# Print the rectangle using stars with R rows and C columns.
+question: # Print the rectangle using stars with R rows and C columns.
 # Sample Input :
 # 3 5
 # Sample Output :
@@ -63,5 +63,41 @@ for i in range(n-2):
     print()
 
 
+question: # Print the star pyramid with the given integer R.
+# Sample Input :5
 
+a = int(input())
+for i in range(1,a+1):
+   for j in range(1, i+1):
+        print("* " ,end="")
+   print()
+
+question: # Print the number half pyramid pattern with the size R.
+# Sample Input : 5
+# Sample Output :
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+n = int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+
+question: # Print the number half pyramid pattern with the size R.
+# Sample Input :5
+# Sample Output :
+# 55555
+# 4444
+# 333
+# 22
+# 1
+n = int(input())
+for i in range(n, 0, -1):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
 
